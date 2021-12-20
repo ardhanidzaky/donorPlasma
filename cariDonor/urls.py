e0abfeb1f8f9a3ff17c0782d82c4f7fdf9e92fd4
Rahadian Panji <rahadianpanjipanji@gmail.com>
2021-12-20 15:34:44 +0700

    add endpoints to filter pendonor

--- cariDonor/urls.py
+++ cariDonor/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', input_cari_donor, name='Cari Donor'),
     path('kota-by-prov', get_kota_by_prov, name='Kota By Prov'),
-    path('all-prov', get_all_prov, name='All Provs'),
+    path('all-prov', get_all_prov, name='All Prov'),
+    path('all-kota', get_all_kota, name='All Kota'),
+    path('json-pendonor', get_pendonor_http, name='JSON Pendonor'),
 ]
--- cariDonor/views.py
+++ cariDonor/views.py
@@ -17,68 +17,84 @@ def input_cari_donor(request):
     goldar = request.GET.get('goldar', None)
 
     if prov and kota and goldar:
-        prov = Provinsi.objects.get(nama=prov)
-        kota = Kota.objects.get(nama=kota, provinsi=prov)
-        try:
-            if goldar == "all":
-                donor = Pendonor.objects.filter(provinsi=prov, kota=kota)
-            else:
-                donor = Pendonor.objects.filter(provinsi=prov, kota=kota, golongan_darah=goldar)
-        except Pendonor.DoesNotExist:
-            donor = None
+        donor = get_pendonor_json(request)
+        donor = json.loads(donor)
         ctx = {"pendonor": donor, "kota": kota, "goldar": goldar}
         return render(request, 'result-pendonor.html', ctx)
 
-    prov = Provinsi.objects.all().values()
-    if len(prov) < 34:
-        addKotaProvinsi()
+    check_prov_kota()
     form = cariDonor(request.POST or None)
     if form.is_valid():
         return HttpResponseRedirect(f"/cari-donor?provinsi={form.provinsi.nama}&kota={form.kota.nama}&goldar={form.goldar}")
     return render(request, 'cari-donor-filter.html')
 
-def get_prov_by_name(prov):
-    if not prov:
-        return None
-    prov = Provinsi.objects.get(nama=prov)
-    return prov
-
-def get_kota_by_name(kota):
-    if not kota:
-        return None
-    kota = Kota.objects.get(nama=kota)
-    return kota
-
-# def get_prov_by_name(request):
-#     prov = request.GET.get('name', None)
-#     if not prov:
-#         return HttpResponse("None", content_type="application/json")
-#     prov = Provinsi.objects.get(nama=prov)
-#     data = serializers.serialize('json', prov)
-#     return HttpResponse(data, content_type="application/json")
-
-# def get_kota_by_name(request):
-#     kota = request.GET.get('name', None)
-#     if not kota:
-#         return HttpResponse("None", content_type="application/json")
-#     kota = Kota.objects.get(nama=kota)
-#     data = serializers.serialize('json', kota)
-#     return HttpResponse(data, content_type="application/json")
-
 def get_all_prov(request):
+    check_prov_kota()
     prov = Provinsi.objects.order_by('nama')
     data = serializers.serialize('json', prov)
     return HttpResponse(data, content_type="application/json")
 
+def get_all_kota(request):
+    check_prov_kota()
+    prov = Provinsi.objects.order_by('nama')
+    data = {}
+    for i in prov:
+        data[i.nama] = [j.nama for j in i.kota_set.all()]
+    data = json.dumps(data)
+    return HttpResponse(data, content_type="application/json")
+
 def get_kota_by_prov(request):
+    check_prov_kota()
     prov = request.GET.get('provinsi', None)
     if not prov:
         return HttpResponse("None", content_type="application/json")
-    print("prov in params:", prov)
     prov = Provinsi.objects.get(nama=prov)
     kota = prov.kota_set.all()
     data = serializers.serialize('json', kota)
     return HttpResponse(data, content_type="application/json")
+
+def get_pendonor_json(request):
+    check_prov_kota()
+    prov = request.GET.get('provinsi', None)
+    kota = request.GET.get('kota', None)
+    goldar = request.GET.get('goldar', None)
+
+    prov = Provinsi.objects.get(nama=prov)
+    kota = Kota.objects.get(nama=kota, provinsi=prov)
+    try:
+        if goldar == "all":
+            donor = Pendonor.objects.filter(provinsi=prov, kota=kota)
+        else:
+            donor = Pendonor.objects.filter(provinsi=prov, kota=kota, golongan_darah=goldar)
+    except Pendonor.DoesNotExist:
+        donor = []
+    
+    donor = serializers.serialize('json', donor)
+    return donor
+
+def get_pendonor_http(request):
+    check_prov_kota()
+    prov = request.GET.get('provinsi', None)
+    kota = request.GET.get('kota', None)
+    goldar = request.GET.get('goldar', None)
+
+    prov = Provinsi.objects.get(nama=prov)
+    kota = Kota.objects.get(nama=kota, provinsi=prov)
+    try:
+        if goldar == "all":
+            donor = Pendonor.objects.filter(provinsi=prov, kota=kota)
+        else:
+            donor = Pendonor.objects.filter(provinsi=prov, kota=kota, golongan_darah=goldar)
+    except Pendonor.DoesNotExist:
+        donor = []
+    
+    donor = serializers.serialize('json', donor)
+    return HttpResponse(donor, content_type="application/json")
+
+def check_prov_kota():
+    prov = Provinsi.objects.all().values()
+    if len(prov) < 34:
+        addKotaProvinsi()
 
 def addKotaProvinsi():
     daftar = {'Aceh': ['Kabupaten Aceh Barat', 'Kabupaten Aceh Barat Daya', 'Kabupaten Aceh Besar', 'Kabupaten Aceh Jaya', 'Kabupaten Aceh Selatan', 'Kabupaten Aceh Singkil', 'Kabupaten Aceh Tamiang', 'Kabupaten Aceh Tengah', 'Kabupaten Aceh Tenggara', 'Kabupaten Aceh Timur', 'Kabupaten Aceh Utara', 'Kabupaten Bener Meriah', 'Kabupaten Bireuen', 'Kabupaten Gayo Lues', 'Kabupaten Nagan Raya', 'Kabupaten Pidie', 'Kabupaten Pidie Jaya', 'Kabupaten Simeulue', 'Kota Banda Aceh', 'Kota Langsa', 'Kota Lhokseumawe', 'Kota Sabang', 'Kota Subulussalam'], 'Sumatra Utara': ['Kabupaten Asahan', 'Kabupaten Batu Bara', 'Kabupaten Dairi', 'Kabupaten Deli Serdang', 'Kabupaten Humbang Hasundutan', 'Kabupaten Karo', 'Kabupaten Labuhanbatu', 'Kabupaten Labuhanbatu Selatan', 'Kabupaten Labuhanbatu Utara', 'Kabupaten Langkat', 'Kabupaten Mandailing Natal', 'Kabupaten Nias', 'Kabupaten Nias Barat', 'Kabupaten Nias Selatan', 'Kabupaten Nias Utara', 'Kabupaten Padang Lawas', 'Kabupaten Padang Lawas Utara', 'Kabupaten Pakpak Bharat', 'Kabupaten Samosir', 'Kabupaten Serdang Bedagai', 'Kabupaten Simalungun', 'Kabupaten Tapanuli Selatan', 'Kabupaten Tapanuli Tengah', 'Kabupaten Tapanuli Utara', 'Kabupaten Toba', 'Kota Binjai', 'Kota Gunungsitoli', 'Kota Medan', 'Kota Padang Sidempuan', 'Kota Pematangsiantar', 'Kota Sibolga', 'Kota Tanjungbalai', 'Kota Tebing Tinggi'], 'Sumatra Barat': ['Kabupaten Agam', 'Kabupaten Dharmasraya', 'Kabupaten Kepulauan Mentawai', 'Kabupaten Lima Puluh Kota', 'Kabupaten Padang Pariaman', 'Kabupaten Pasaman', 'Kabupaten Pasaman Barat', 'Kabupaten Pesisir Selatan', 'Kabupaten Sijunjung', 'Kabupaten Solok', 'Kabupaten Solok Selatan', 'Kabupaten Tanah Datar', 'Kota Bukittinggi', 'Kota Padang', 'Kota Padang Panjang', 'Kota Pariaman', 'Kota Payakumbuh', 'Kota Sawahlunto', 'Kota Solok'], 'Riau': ['Kabupaten Bengkalis', 'Kabupaten Indragiri Hilir', 'Kabupaten Indragiri Hulu', 'Kabupaten Kampar', 'Kabupaten Kepulauan Meranti', 'Kabupaten Kuantan Singingi', 'Kabupaten Pelalawan', 'Kabupaten Rokan Hilir', 'Kabupaten Rokan Hulu', 'Kabupaten Siak', 'Kota Dumai', 'Kota Pekanbaru'], 'Kepulauan Riau': ['Kabupaten Bintan', 'Kabupaten Karimun', 'Kabupaten Kepulauan Anambas', 'Kabupaten Lingga', 'Kabupaten Natuna', 'Kota Batam', 'Kota Tanjung Pinang'], 'Jambi': ['Kabupaten Batanghari', 'Kabupaten Bungo', 'Kabupaten Kerinci', 'Kabupaten Merangin', 'Kabupaten Muaro Jambi', 'Kabupaten Sarolangun', 'Kabupaten Tanjung Jabung Barat', 'Kabupaten Tanjung Jabung Timur', 'Kabupaten Tebo', 'Kota Jambi', 'Kota Sungai Penuh'], 'Bengkulu': ['Kabupaten Bengkulu Selatan', 'Kabupaten Bengkulu Tengah', 'Kabupaten Bengkulu Utara', 'Kabupaten Kaur', 'Kabupaten Kepahiang', 'Kabupaten Lebong', 'Kabupaten Mukomuko', 'Kabupaten Rejang Lebong', 'Kabupaten Seluma', 'Kota Bengkulu'], 'Sumatra Selatan': ['Kabupaten Banyuasin', 'Kabupaten Empat Lawang', 'Kabupaten Lahat', 'Kabupaten Muara Enim', 'Kabupaten Musi Banyuasin', 'Kabupaten Musi Rawas', 'Kabupaten Musi Rawas Utara', 'Kabupaten Ogan Ilir', 'Kabupaten Ogan Komering Ilir', 'Kabupaten Ogan Komering Ulu', 'Kabupaten Ogan Komering Ulu Selatan', 'Kabupaten Ogan Komering Ulu Timur', 'Kabupaten Penukal Abab Lematang Ilir', 'Kota Lubuklinggau', 'Kota Pagar Alam', 'Kota Palembang', 'Kota Prabumulih'], 'Kepulauan Bangka Belitung': ['Kabupaten Bangka', 'Kabupaten Bangka Barat', 'Kabupaten Bangka Selatan', 'Kabupaten Bangka Tengah', 'Kabupaten Belitung', 'Kabupaten Belitung Timur', 'Kota Pangkalpinang'], 'Lampung': ['Kabupaten Lampung Barat', 'Kabupaten Lampung Selatan', 'Kabupaten Lampung Tengah', 'Kabupaten Lampung Timur', 'Kabupaten Lampung Utara', 'Kabupaten Mesuji', 'Kabupaten Pesawaran', 'Kabupaten Pesisir Barat', 'Kabupaten Pringsewu', 'Kabupaten Tanggamus', 'Kabupaten Tulang Bawang', 'Kabupaten Tulang Bawang Barat', 'Kabupaten Way Kanan', 'Kota Bandar Lampung', 'Kota Metro'], 'Banten': ['Kabupaten Lebak', 'Kabupaten Pandeglang', 'Kabupaten Serang', 'Kabupaten Tangerang', 'Kota Cilegon', 'Kota Serang', 'Kota Tangerang', 'Kota Tangerang Selatan'], 'Jawa Barat': ['Kabupaten Bandung', 'Kabupaten Bandung Barat', 'Kabupaten Bekasi', 'Kabupaten Bogor', 'Kabupaten Ciamis', 'Kabupaten Cianjur', 'Kabupaten Cirebon', 'Kabupaten Garut', 'Kabupaten Indramayu', 'Kabupaten Karawang', 'Kabupaten Kuningan', 'Kabupaten Majalengka', 'Kabupaten Pangandaran', 'Kabupaten Purwakarta', 'Kabupaten Subang', 'Kabupaten Sukabumi', 'Kabupaten Sumedang', 'Kabupaten Tasikmalaya', 'Kota Bandung', 'Kota Banjar', 'Kota Bekasi', 'Kota Bogor', 'Kota Cimahi', 'Kota Cirebon', 'Kota Depok', 'Kota Sukabumi', 'Kota Tasikmalaya'], 'Jakarta': ['Kabupaten Administrasi Kepulauan Seribu', 'Kota Administrasi Jakarta Barat', 'Kota Administrasi Jakarta Pusat', 'Kota Administrasi Jakarta Selatan', 'Kota Administrasi Jakarta Timur', 'Kota Administrasi Jakarta Utara'], 'Jawa Tengah': ['Kabupaten Banjarnegara', 'Kabupaten Banyumas', 'Kabupaten Batang', 'Kabupaten Blora', 'Kabupaten Boyolali', 'Kabupaten Brebes', 'Kabupaten Cilacap', 'Kabupaten Demak', 'Kabupaten Grobogan', 'Kabupaten Jepara', 'Kabupaten Karanganyar', 'Kabupaten Kebumen', 'Kabupaten Kendal', 'Kabupaten Klaten', 'Kabupaten Kudus', 'Kabupaten Magelang', 'Kabupaten Pati', 'Kabupaten Pekalongan', 'Kabupaten Pemalang', 'Kabupaten Purbalingga', 'Kabupaten Purworejo', 'Kabupaten Rembang', 'Kabupaten Semarang', 'Kabupaten Sragen', 'Kabupaten Sukoharjo', 'Kabupaten Tegal', 'Kabupaten Temanggung', 'Kabupaten Wonogiri', 'Kabupaten Wonosobo', 'Kota Magelang', 'Kota Pekalongan', 'Kota Salatiga', 'Kota Semarang', 'Kota Surakarta', 'Kota Tegal'], 'Yogyakarta': ['Kabupaten Bantul', 'Kabupaten Gunungkidul', 'Kabupaten Kulon Progo', 'Kabupaten Sleman', 'Kota Yogyakarta'], 'Jawa Timur': ['Kabupaten Bangkalan', 'Kabupaten Banyuwangi', 'Kabupaten Blitar', 'Kabupaten Bojonegoro', 'Kabupaten Bondowoso', 'Kabupaten Gresik', 'Kabupaten Jember', 'Kabupaten Jombang', 'Kabupaten Kediri', 'Kabupaten Lamongan', 'Kabupaten Lumajang', 'Kabupaten Madiun', 'Kabupaten Magetan', 'Kabupaten Malang', 'Kabupaten Mojokerto', 'Kabupaten Nganjuk', 'Kabupaten Ngawi', 'Kabupaten Pacitan', 'Kabupaten Pamekasan', 'Kabupaten Pasuruan', 'Kabupaten Ponorogo', 'Kabupaten Probolinggo', 'Kabupaten Sampang', 'Kabupaten Sidoarjo', 'Kabupaten Situbondo', 'Kabupaten Sumenep', 'Kabupaten Trenggalek', 'Kabupaten Tuban', 'Kabupaten Tulungagung', 'Kota Batu', 'Kota Blitar', 'Kota Kediri', 'Kota Madiun', 'Kota Malang', 'Kota Mojokerto', 'Kota Pasuruan', 'Kota Probolinggo', 'Kota Surabaya'], 'Bali': ['Kabupaten Badung', 'Kabupaten Bangli', 'Kabupaten Buleleng', 'Kabupaten Gianyar', 'Kabupaten Jembrana', 'Kabupaten Karangasem', 'Kabupaten Klungkung', 'Kabupaten Tabanan', 'Kota Denpasar'], 'Nusa Tenggara Barat': ['Kabupaten Bima', 'Kabupaten Dompu', 'Kabupaten Lombok Barat', 'Kabupaten Lombok Tengah', 'Kabupaten Lombok Timur', 'Kabupaten Lombok Utara', 'Kabupaten Sumbawa', 'Kabupaten Sumbawa Barat', 'Kota Bima', 'Kota Mataram'], 'Nusa Tenggara Timur': ['Kabupaten Alor', 'Kabupaten Belu', 'Kabupaten Ende', 'Kabupaten Flores Timur', 'Kabupaten Kupang', 'Kabupaten Lembata', 'Kabupaten Malaka', 'Kabupaten Manggarai', 'Kabupaten Manggarai Barat', 'Kabupaten Manggarai Timur', 'Kabupaten Nagekeo', 'Kabupaten Ngada', 'Kabupaten Rote Ndao', 'Kabupaten Sabu Raijua', 'Kabupaten Sikka', 'Kabupaten Sumba Barat', 'Kabupaten Sumba Barat Daya', 'Kabupaten Sumba Tengah', 'Kabupaten Sumba Timur', 'Kabupaten Timor Tengah Selatan', 'Kabupaten Timor Tengah Utara', 'Kota Kupang'], 'Kalimantan Barat': ['Kabupaten Bengkayang', 'Kabupaten Kapuas Hulu', 'Kabupaten Kayong Utara', 'Kabupaten Ketapang', 'Kabupaten Kubu Raya', 'Kabupaten Landak', 'Kabupaten Melawi', 'Kabupaten Mempawah', 'Kabupaten Sambas', 'Kabupaten Sanggau', 'Kabupaten Sekadau', 'Kabupaten Sintang', 'Kota Pontianak', 'Kota Singkawang'], 'Kalimantan Selatan': ['Kabupaten Balangan', 'Kabupaten Banjar', 'Kabupaten Barito Kuala', 'Kabupaten Hulu Sungai Selatan', 'Kabupaten Hulu Sungai Tengah', 'Kabupaten Hulu Sungai Utara', 'Kabupaten Kotabaru', 'Kabupaten Tabalong', 'Kabupaten Tanah Bumbu', 'Kabupaten Tanah Laut', 'Kabupaten Tapin', 'Kota Banjarbaru', 'Kota Banjarmasin'], 'Kalimantan Tengah': ['Kabupaten Barito Selatan', 'Kabupaten Barito Timur', 'Kabupaten Barito Utara', 'Kabupaten Gunung Mas', 'Kabupaten Kapuas', 'Kabupaten Katingan', 'Kabupaten Kotawaringin Barat', 'Kabupaten Kotawaringin Timur', 'Kabupaten Lamandau', 'Kabupaten Murung Raya', 'Kabupaten Pulang Pisau', 'Kabupaten Sukamara', 'Kabupaten Seruyan', 'Kota Palangka Raya'], 'Kalimantan Timur': ['Kabupaten Berau', 'Kabupaten Kutai Barat', 'Kabupaten Kutai Kartanegara', 'Kabupaten Kutai Timur', 'Kabupaten Mahakam Ulu', 'Kabupaten Paser', 'Kabupaten Penajam Paser Utara', 'Kota Balikpapan', 'Kota Bontang', 'Kota Samarinda'], 'Kalimantan Utara': ['Kabupaten Bulungan', 'Kabupaten Malinau', 'Kabupaten Nunukan', 'Kabupaten Tana Tidung', 'Kota Tanjung Selor', 'Kota Tarakan'], 'Gorontalo': ['Kabupaten Boalemo', 'Kabupaten Bone Bolango', 'Kabupaten Gorontalo', 'Kabupaten Gorontalo Utara', 'Kabupaten Pohuwato', 'Kota Gorontalo'], 'Sulawesi Barat': ['Kabupaten Majene', 'Kabupaten Mamasa', 'Kabupaten Mamuju', 'Kabupaten Mamuju Tengah', 'Kabupaten Pasangkayu', 'Kabupaten Polewali Mandar', 'Kota Mamuju'], 'Sulawesi Selatan': ['Kabupaten Bantaeng', 'Kabupaten Barru', 'Kabupaten Bone', 'Kabupaten Bulukumba', 'Kabupaten Enrekang', 'Kabupaten Gowa', 'Kabupaten Jeneponto', 'Kabupaten Kepulauan Selayar', 'Kabupaten Luwu', 'Kabupaten Luwu Timur', 'Kabupaten Luwu Utara', 'Kabupaten Maros', 'Kabupaten Pangkajene dan Kepulauan', 'Kabupaten Pinrang', 'Kabupaten Sidenreng Rappang', 'Kabupaten Sinjai', 'Kabupaten Soppeng', 'Kabupaten Takalar', 'Kabupaten Tana Toraja', 'Kabupaten Toraja Utara', 'Kabupaten Wajo', 'Kota Makassar', 'Kota Palopo', 'Kota Parepare'], 'Sulawesi Tenggara': ['Kabupaten Bombana', 'Kabupaten Buton', 'Kabupaten Buton Selatan', 'Kabupaten Buton Tengah', 'Kabupaten Buton Utara', 'Kabupaten Kolaka', 'Kabupaten Kolaka Timur', 'Kabupaten Kolaka Utara', 'Kabupaten Konawe', 'Kabupaten Konawe Kepulauan', 'Kabupaten Konawe Selatan', 'Kabupaten Konawe Utara', 'Kabupaten Muna', 'Kabupaten Muna Barat', 'Kabupaten Wakatobi', 'Kota Baubau', 'Kota Kendari'], 'Sulawesi Tengah': ['Kabupaten Banggai', 'Kabupaten Banggai Kepulauan', 'Kabupaten Banggai Laut', 'Kabupaten Buol', 'Kabupaten Donggala', 'Kabupaten Morowali', 'Kabupaten Morowali Utara', 'Kabupaten Parigi Moutong', 'Kabupaten Poso', 'Kabupaten Sigi', 'Kabupaten Tojo Una-Una', 'Kabupaten Tolitoli', 'Kota Palu'], 'Sulawesi Utara': ['Kabupaten Bolaang Mongondow', 'Kabupaten Bolaang Mongondow Selatan', 'Kabupaten Bolaang Mongondow Timur', 'Kabupaten Bolaang Mongondow Utara', 'Kabupaten Kepulauan Sangihe', 'Kabupaten Kepulauan Siau Tagulandang Biaro', 'Kabupaten Kepulauan Talaud', 'Kabupaten Minahasa', 'Kabupaten Minahasa Selatan', 'Kabupaten Minahasa Tenggara', 'Kabupaten Minahasa Utara', 'Kota Bitung', 'Kota Kotamobagu', 'Kota Manado', 'Kota Tomohon'], 'Maluku': ['Kabupaten Buru', 'Kabupaten Buru Selatan', 'Kabupaten Kepulauan Aru', 'Kabupaten Kepulauan Tanimbar', 'Kabupaten Maluku Barat Daya', 'Kabupaten Maluku Tengah', 'Kabupaten Maluku Tenggara', 'Kabupaten Seram Bagian Barat', 'Kabupaten Seram Bagian Timur', 'Kota Ambon', 'Kota Tual'], 'Maluku Utara': ['Kabupaten Halmahera Barat', 'Kabupaten Halmahera Tengah', 'Kabupaten Halmahera Timur', 'Kabupaten Halmahera Selatan', 'Kabupaten Halmahera Utara', 'Kabupaten Kepulauan Sula', 'Kabupaten Pulau Morotai', 'Kabupaten Pulau Taliabu', 'Kota Ternate', 'Kota Tidore Kepulauan'], 'Papua': ['Kabupaten Asmat', 'Kabupaten Biak Numfor', 'Kabupaten Boven Digoel', 'Kabupaten Deiyai', 'Kabupaten Dogiyai', 'Kabupaten Intan Jaya', 'Kabupaten Jayapura', 'Kabupaten Jayawijaya', 'Kabupaten Keerom', 'Kabupaten Kepulauan Yapen', 'Kabupaten Lanny Jaya', 'Kabupaten Mamberamo Raya', 'Kabupaten Mamberamo Tengah', 'Kabupaten Mappi', 'Kabupaten Merauke', 'Kabupaten Mimika', 'Kabupaten Nabire', 'Kabupaten Nduga', 'Kabupaten Paniai', 'Kabupaten Pegunungan Bintang', 'Kabupaten Puncak', 'Kabupaten Puncak Jaya', 'Kabupaten Sarmi', 'Kabupaten Supiori', 'Kabupaten Tolikara', 'Kabupaten Waropen', 'Kabupaten Yahukimo', 'Kabupaten Yalimo', 'Kota Jayapura', 'Kota Timika'], 'Papua Barat': ['Kabupaten Fakfak', 'Kabupaten Kaimana', 'Kabupaten Manokwari', 'Kabupaten Manokwari Selatan', 'Kabupaten Maybrat', 'Kabupaten Pegunungan Arfak', 'Kabupaten Raja Ampat', 'Kabupaten Sorong', 'Kabupaten Sorong Selatan', 'Kabupaten Tambrauw', 'Kabupaten Teluk Bintuni', 'Kabupaten Teluk Wondama', 'Kota Manokwari', 'Kota Sorong']}
