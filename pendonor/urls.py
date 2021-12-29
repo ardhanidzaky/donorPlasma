ef3af7668e6c34789250e48b29e7c9edaef398e8
Sulthan Afif Althaf <afifalthafs1@gmail.com>
2021-12-29 20:39:56 +0700

    add endpoint pendonor

--- pendonor/urls.py
+++ pendonor/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('json',data_pendonor,name='json'),
     path('delete',delete_pendonor,name='delete_pendonor'),
     path('update',update_pendonor,name='update_pendonor'),
-    path('get_user',get_pendonor, name='get_pendonor')
+    path('get_user',get_pendonor, name='get_pendonor'),
+    path('get_info', info_donor, name='info_donor'),
+    path('add_pendonor', daftar_donor, name='daftar_donor')
 ]
